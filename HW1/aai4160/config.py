fba0e9d2c868c04e11d3770bb927ff6521bb2ac0
watchstep <cathy033077@gmail.com>
2024-03-29 12:57:56 +0000

    4-2 ing

---
+++ HW1/aai4160/config.py
@@ -0,0 +1,15 @@
+import wandb
+
+# Example sweep configuration
+sweep_configuration = {
+    "method": "random",
+    "name": "sweep",
+    "metric": {"goal": "maximize", "name": "val_acc"},
+    "parameters": {
+        "batch_size": {"values": [16, 32, 64]},
+        "epochs": {"values": [5, 10, 15]},
+        "lr": {"max": 0.1, "min": 0.0001},
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="RL-HW1")
