975839e9c867843187d4f2c19630fe700da7ef83
Watch your step! <88659167+watchstep@users.noreply.github.com>
2024-05-08 11:27:36 +0900

    Delete HW3 directory

--- HW3/aai4160/env_configs/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dqn_atari_config import atari_dqn_config
-from .dqn_basic_config import basic_dqn_config
-
-configs = {
-    "dqn_atari": atari_dqn_config,
-    "dqn_basic": basic_dqn_config,
-}
